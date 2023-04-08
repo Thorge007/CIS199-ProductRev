1fa4e12639561e130f40e04fe8b32d7a558231ba
Thorge007 <130246214+Thorge007@users.noreply.github.com>
2023-04-08 15:22:18 -0400

    Add files via upload

---
+++ ProductRev-21S-Start/ProductRev/ProductRev/FuelOrder.cs
@@ -0,0 +1,94 @@
+﻿//Grading ID: S0941
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductRev
+
+{
+    public class FuelOrder
+
+    {
+        // #1 - Put needed backing fields here
+        // Remember: Some properties may be
+        // auto-implemented, so only create
+        // backing fields for ones needed
+        private int _volume;
+        private int _price;
+
+        double Cost;
+
+        public string Description
+        { get; set; }
+
+        public string OctaneRating
+        { get; set; }
+        
+
+        public int Volume
+        {
+            get
+            {
+                return (int)(_volume * 1.1);
+            }
+
+            set
+            {
+                if (value >= 0)
+                    _volume = value;
+                else
+                    _volume = 0;
+            }
+        }
+
+        public double Price
+        {
+            get
+            {
+                return _price;
+            }
+
+            set
+            {
+                if (value >= 0)
+                    _price = (int)value;
+                else
+                    _price = 0;
+            }
+        }
+
+        public FuelOrder(string desc, string octaneRating, double volume, double price)
+        {
+            Description = desc;
+            OctaneRating = octaneRating;
+            Volume = Convert.ToInt32(volume);
+            Price = price;
+
+        }
+
+        public double CalcFuelCost()
+        {
+            Cost = Price * Volume;
+            return Cost;
+        }
+
+        public override string ToString()
+        {
+            // #8 - Put your code here to create formatted string
+            // Use format that would produce a string like:
+            // "Premium: 92 (25 @ $2.99)"
+            // where 92 is the octane rating, Premium is the
+            // description, 25 is the volume, and $2.99 is
+            // the price. No newlines are needed.
+            string objectString;
+
+            objectString = $"{"Premium:  " + OctaneRating + "(" + Volume + "@" + "$" + Price +}";
+
+            return objectString;
+
+        }
+    }
+}
---
+++ ProductRev-21S-Start/ProductRev/ProductRev/Program.cs
@@ -0,0 +1,47 @@
+﻿//Grading ID: S0941
+
+using System;
+//Grading ID: S0941
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ProductRev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double currentValue;   // Current product's value
+            double totalValue = 0; // Running total of product's value
+            FuelOrder[] orders; // Product Items
+
+            // #1 - Put code here to construct 3  Fuel orders
+            FuelOrder order1
+            FuelOrder order2
+            FuelOrder order3
+
+            orders = new FuelOrder[] { order1, order2, order3 };
+
+            WriteLine($"Product Revenue Report:");
+
+            // #2 - Complete loop to step through array of items
+            for (int index = 0; index <= orders.Length-1; ++index)
+            {
+
+                currentValue = FuelOrder.CalcFuelCost();
+
+                totalValue += currentValue;
+
+                // #5 - Output currentValue, with currency formatting, followed by string representation
+                //      of current item from array
+            }
+
+            WriteLine("\nTotal Gross Revenue:" + totalValue.ToString("C"));
+            // #6 - Output totalValue of Gross Revenue, with currency formatting
+
+        }
+    }
+}
